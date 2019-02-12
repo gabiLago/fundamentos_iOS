e4bacee21d08bfcf7a22cac983de5496d024adb4
Gabi Lago Blasco <gabilagoblasco.es@gmail.com>
2019-02-12 16:42:55 +0100

    Seasons and Episodes

---
+++ Westeros/Domain/Episode.swift
@@ -0,0 +1,9 @@
+//
+//  Episode.swift
+//  Westeros
+//
+//  Created by Gabriel Lago Blasco on 12/02/2019.
+//  Copyright © 2019 Gabi Lago Blasco. All rights reserved.
+//
+
+import Foundation
---
+++ Westeros/Domain/Season.swift
@@ -0,0 +1,9 @@
+//
+//  Season.swift
+//  Westeros
+//
+//  Created by Gabriel Lago Blasco on 12/02/2019.
+//  Copyright © 2019 Gabi Lago Blasco. All rights reserved.
+//
+
+import Foundation
